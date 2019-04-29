17757187c02347362cb6f518cefd8a57af0d17dc
msunkist99 <msunkist99@gmail.com>
2019-04-29 18:36:06 -0500

    2019/04/29 initial code

---
+++ CoderGirl-School/Course.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoderGirl_School
+{
+    public class Course
+    {
+        string name;
+        int credits;
+        int maxStudents;
+        List<Student> students;
+
+        public string Name {
+            get { return name; }
+            internal set { name = value; }
+        }
+
+        public int Credits {
+            get { return credits; }
+            internal set { credits = value; }
+        }
+
+        public int MaxStudents {
+            get { return maxStudents; }
+            internal set { maxStudents = value; }
+        }
+
+        public List<Student> Students {
+            get { return students; }
+            internal set { students = value; }
+        }
+    }
+}
---
+++ CoderGirl-School/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoderGirl_School
+{
+    public class Student
+    {
+        string name;
+        int studentId;
+        int numberOfCredits;
+        double gpa;
+
+        public string Name
+        {
+            get { return name; }
+            internal set { name = value; }
+        }
+
+        public int StudentId
+        {
+            get { return studentId; }
+            internal set { studentId = value; }
+        }
+
+        public int NumberOfCredits
+        {
+            get { return numberOfCredits; }
+            internal set { numberOfCredits = value; }
+        }
+
+        public double GPA
+        {
+            get { return gpa; }
+            set { gpa = value; }
+        }
+
+    }
+}
