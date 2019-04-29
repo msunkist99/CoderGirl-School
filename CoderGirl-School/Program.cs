b0a9967bce396dc06c9d38f8f037054072f66098
msunkist99 <msunkist99@gmail.com>
2019-04-29 17:11:36 -0500

    Add project files.

---
+++ CoderGirl-School/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoderGirl_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
